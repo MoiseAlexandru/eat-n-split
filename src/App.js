73fdd85ed8f874534b03b1f5ac591ededb2af3b0
Moise Alexandru <moise.alexandru.ichb@gmail.com>
2023-11-07 04:11:14 +0200

    fixat faptul ca nu se schimbau prop-urile cand se schimba prietenul

--- src/App.js
+++ src/App.js
@@ -182,7 +182,7 @@ function App() {
                 {showAddFriend && <FormAddFriend onAddFriend = {handleAddFriend}/>}
                 <Button onClick = {handleShowAddFriend}> {showAddFriend ? "Close" : "Add friend"} </Button>
             </div>
-            {selectedFriend && <FormSplitBill selectedFriend = {selectedFriend} onSplitBill = {handleSplitBill}/>}
+            {selectedFriend && <FormSplitBill selectedFriend = {selectedFriend} onSplitBill = {handleSplitBill} key = {selectedFriend.id}/>}
         </div>
     );
 }
